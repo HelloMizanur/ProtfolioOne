2bfce830045ea957a453a36dd332217927f0a19e
MizanurRahman09 <mizanur.rahman099990@gmail.com>
2024-11-04 21:13:52 +0600

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,79 @@
+import PropTypes from "prop-types";
+// Button Primary
+const ButtonPrimary = ({ href, target = "_self", label, icon, classes }) => {
+  const isPdf = href && href.endsWith(".pdf");
+  if (href) {
+    return (
+      <a
+        href={href}
+        target={target}
+        className={"btn btn-primary " + classes}
+        download={isPdf ? true : undefined}
+      >
+        {label}
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </a>
+    );
+  } else {
+    return (
+      <button className={"btn btn-primary " + classes}>
+        {label}
+
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </button>
+    );
+  }
+};
+ButtonPrimary.propTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+// Button Outline
+
+const ButtonOutline = ({ href, target = "_self", label, icon, classes }) => {
+  if (href) {
+    return (
+      <a href={href} target={target} className={"btn btn-outline " + classes}>
+        {label}
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </a>
+    );
+  } else {
+    return (
+      <button className={"btn btn-outline " + classes}>
+        {label}
+
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </button>
+    );
+  }
+};
+ButtonOutline.propTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+export { ButtonPrimary, ButtonOutline };
